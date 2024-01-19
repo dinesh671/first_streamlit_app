e92e1784efe48054fc6578e82fed0c02cbf57552
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-01-19 15:03:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
-import streamlit
+import streamlit as s
 
-streamlit.title('I am dinesh kumar looking forward to work on streamlit')
+s.title('I am dinesh kumar looking forward to work on streamlit')
+
+s.header('Breakfast Menu')
+
+s.text('Omega 3 & Blueberry Oatmeal')
+s.text('Kale, Spinach & Rocket Smoothie')
+s.text('Hard-Boiled Free-Range Egg')
