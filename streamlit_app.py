4334d33e352f081967a2c6626b01b8e7d7e17e5b
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-01-19 15:09:23 +0530

    Update streamlit_app.py
    added more items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit as s
 s.title('I am dinesh kumar looking forward to work on streamlit')
 
 s.header('Breakfast Menu')
+s.text('🥣 Omega 3 & Blueberry Oatmeal')
+s.text('🥗 Kale, Spinach & Rocket Smoothie')
+s.text('🐔 Hard-Boiled Free-Range Egg')
+s.text('🥑🍞 Avocada Toast')
 
-s.text('Omega 3 & Blueberry Oatmeal')
-s.text('Kale, Spinach & Rocket Smoothie')
-s.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
