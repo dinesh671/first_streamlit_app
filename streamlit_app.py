4fc43e4a35861dab192f4660105ec43121d8849d
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-01-19 15:20:41 +0530

    error resolved i guess

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as s
 import pandas
 
-my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 s.title('my_fruit_list')
 
 s.header('Breakfast Menu')
