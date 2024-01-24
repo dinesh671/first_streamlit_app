0e2421c5dc2e859778546a3741068498e7284dc5
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-01-24 11:34:57 +0530

    commented the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as s
 import pandas
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
+# import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response)
 
