15cdd9a90ffa09e4d94b26acc83b52a231fa3c43
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-01-24 11:36:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as s
 import pandas
 
-streamlit.header("Fruityvice Fruit Advice!")
-# import requests
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response)
+s.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
