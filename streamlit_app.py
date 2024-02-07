d2789390e0a38c8a1ad5f4e7d2e56f41987ac36a
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-02-07 15:10:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ import snowflake.connector
 
 s.header("The Fruit load list contains:")
 
-s.datafame(my_fruit_list)
-add_my_fruit = s.text_input('What fruit would you like to add?','jackfruit')
-s.write('Thanks for adding', add_my_fruit)
+# s.datafame(my_fruit_list)
+# add_my_fruit = s.text_input('What fruit would you like to add?','jackfruit')
+# s.write('Thanks for adding', add_my_fruit)
