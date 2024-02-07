46eec5ead96cf6a9172922ee95eef38048bdbf24
DVS Dinesh Kumar <63796652+dinesh671@users.noreply.github.com>
2024-02-07 15:30:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,30 @@
-import streamlit as s
+import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-s.title('my_fruit_list')
+streamlit.title('my_fruit_list')
 
-s.header('Breakfast Menu')
-s.text('🥣 Omega 3 & Blueberry Oatmeal')
-s.text('🥗 Kale, Spinach & Rocket Smoothie')
-s.text('🐔 Hard-Boiled Free-Range Egg')
-s.text('🥑🍞 Avocada Toast')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocada Toast')
 
-s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = s.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-s.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
-s.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = s.text_input('What fruit would you like information about?','Kiwi')
-s.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
@@ -32,19 +32,19 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-s.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**s.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-s.text("Hello from Snowflake:")
-s.text(my_data_row)
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
-s.header("The Fruit load list contains:")
+streamlit.header("The Fruit load list contains:")
 
-# s.datafame(my_fruit_list)
-# add_my_fruit = s.text_input('What fruit would you like to add?','jackfruit')
-# s.write('Thanks for adding', add_my_fruit)
+# streamlit.datafame(my_fruit_list)
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+# streamlit.write('Thanks for adding', add_my_fruit)
